29e003f17bd07896467a65831afd8035591cc9f3
Benjamin <benjamin.bouguet@insa-rennes.fr>
2013-12-30 15:14:38 +0100

    Création du monteur de carte vide (à utiliser dans l'éditeur Abde)

---
+++ Projet Poo/Carte/MonteurVide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modele
+{
+    public class MonteurVide : MonteurCarte
+    {
+        public MonteurVide()
+            : base(new Vide())
+        {
+
+        }
+        public override void creerCarte()
+        {
+            carte = new CarteClassique();
+            carte.Largeur = 5;
+            carte.Hauteur = 5;
+            creerStructureCarte();
+        }
+    }
+}
---
+++ Projet Poo/Carte/Vide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modele
+{
+    public class Vide : ICreationCarte
+    {
+        public void chargerCarte(ref Carte c)
+        {
+            for (int i = 0; i < c.Largeur; i++)
+            {
+                for (int j=0;j<c.Hauteur;j++)
+                {
+                    c.setCase(i, j, c.FabriqueCase.getCase((int)Case.CaseInt.Plaine));
+                }
+            }
+        }
+    }
+}
